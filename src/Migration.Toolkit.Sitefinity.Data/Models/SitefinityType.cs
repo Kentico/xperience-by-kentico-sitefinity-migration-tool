1d583fe241240fd8e34bac67505ae7708c976719
Nathanel Chadwick <nchadwick@bluemodus.com>
2024-07-18 16:59:26 -0400

    Additional summary comments added

--- src/Migration.Toolkit.Sitefinity.Data/Models/SitefinityType.cs
+++ src/Migration.Toolkit.Sitefinity.Data/Models/SitefinityType.cs
@@ -4,11 +4,38 @@
 /// </summary>
 public abstract class SitefinityType : ISitefinityModel
 {
+    /// <summary>
+    /// The display name of the Sitefinity type.
+    /// </summary>
     public string? DisplayName { get; set; }
+
+    /// <summary>
+    /// The name of the Sitefinity type.
+    /// </summary>
     public abstract string? Name { get; }
+
+    /// <summary>
+    /// The ID of the Sitefinity type.
+    /// </summary>
     public Guid Id { get; set; }
+
+    /// <summary>
+    /// The list of fields associated with the Sitefinity type.
+    /// </summary>
     public List<Field>? Fields { get; set; }
+
+    /// <summary>
+    /// The last modified date of the Sitefinity type.
+    /// </summary>
     public DateTime? LastModified { get; set; }
+
+    /// <summary>
+    /// The class namespace of the Sitefinity type.
+    /// </summary>
     public abstract string? ClassNamespace { get; }
+
+    /// <summary>
+    /// The ID of the parent module type.
+    /// </summary>
     public Guid? ParentModuleTypeId { get; set; }
 }
--- src/Migration.Toolkit.Sitefinity.Data/Models/StaticSitefinityType.cs
+++ src/Migration.Toolkit.Sitefinity.Data/Models/StaticSitefinityType.cs
@@ -4,8 +4,23 @@
 /// </summary>
 public class StaticSitefinityType : SitefinityType
 {
+    /// <summary>
+    /// The class name.
+    /// </summary>
     public string? ClassName { get; set; }
+
+    /// <summary>
+    /// The name of the type.
+    /// </summary>
     public override string? Name => ClassName;
+
+    /// <summary>
+    /// The namespace.
+    /// </summary>
     public string? Namespace { get; set; }
+
+    /// <summary>
+    /// The namespace of the class.
+    /// </summary>
     public override string? ClassNamespace => Namespace;
 }
